cad1a4bcedc3db4241afc086698fb04c9ce35ec1
Nikita Golubev <39840774+TheFunnyDay@users.noreply.github.com>
2021-03-09 23:47:24 +0500

    upd

--- src/searchset.js
+++ src/searchset.js
@@ -5,9 +5,46 @@ $(".see-bg").css({
     "background-image": `url(${localStorage.getItem("customAppBg") ||  "https://wallpaperaccess.com/full/692083.jpg"})`,
 });
 
+document.getElementById('userBg').value = localStorage.getItem("customAppBg");
 
 save.onclick = function(){
     let settingsBg = document.getElementById('userBg').value;
     localStorage.setItem('customAppBg', settingsBg);
     location.reload();
 };
+
+
+searchGoogle.onclick = function(){
+    localStorage.setItem('csearchEng', 'https://google.com');
+};
+searchYandex.onclick = function(){
+    localStorage.setItem('csearchEng', 'https://yandex.ru');
+};
+searchDuckDuckGo.onclick = function(){
+    localStorage.setItem('csearchEng', 'https://duckduckgo.com/');
+};
+
+$('.buttoneng').on('click', this, function(){
+    $('.buttoneng').removeClass('active');
+    $(this).addClass('active');
+});
+function check() {
+    if (localStorage.getItem('csearchEng') == 'https://google.com') {
+        $('#searchGoogle').css({
+            'background' : '#830000'
+        })
+    };
+
+    if (localStorage.getItem('csearchEng') == 'https://yandex.ru') {
+        $('#searchYandex').css({
+            'background' : '#830000'
+        })
+    };
+
+    if (localStorage.getItem('csearchEng') == 'https://duckduckgo.com/') {
+        $('#searchDuckDuckGo').css({
+            'background' : '#830000'
+        })
+    };
+};
+check();
--- src/searctscripts.js
+++ src/searctscripts.js
@@ -1,8 +1,9 @@
 $("#background").css({
     "background-image": `url(${localStorage.getItem("customAppBg") ||  "https://wallpaperaccess.com/full/692083.jpg"})`,
 });
+$('form').attr('action', `${localStorage.getItem('csearchEng')}`);
 
 var q = "TheFunnyDay";
 document.getElementById('submit').onclick = function() {
-    window.open('https://' + 'google.com/search?q=' + q);
+    window.open('https://' + `${localStorage.getItem('csearchEng')}` + '/search'  + q);
 };
